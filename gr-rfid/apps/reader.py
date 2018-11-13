f37c051fce21b8f26e51f52a31fbc30ae902709a
Jeong Sin-Gi <jsgnwk@naver.com>
2018-11-13 10:55:10 +0900

    Update gr-rfid/apps/reader.py

--- gr-rfid/apps/reader.py
+++ gr-rfid/apps/reader.py
@@ -52,7 +52,7 @@ class reader_top_block(gr.top_block):
     ######## Variables #########
     self.dac_rate = 1e6                 # DAC rate
     self.adc_rate = 100e6/50            # ADC rate (2MS/s complex samples)
-    self.decim     = 2                    # Decimation (downsampling factor)
+    self.decim     = 4                    # Decimation (downsampling factor)
     self.ampl     = 0.55                  # Output signal amplitude (signal power vary for different RFX900 cards)
     self.freq     = 910e6                # Modulation frequency (can be set between 902-920)
     self.rx_gain   = 0                   # RX Gain (gain at receiver)
