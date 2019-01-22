3a4f4bb7f279d20ca0ef8a45d4ab66383ac39974
Jeong Sin-Gi <jsgnwk@naver.com>
2019-01-22 17:26:00 +0900

    Revert "Revert "test""
    This reverts commit f06a36d9a6603f91d55f535aaf26b7e6449742b0.

---
+++ git.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+git init
+git remote add origin https://github.com/SpiritFlag/Gen2-UHF-RFID-Reader-with-WISP
---
+++ gr-rfid/apps/build.sh
@@ -0,0 +1,7 @@
+git pull origin decoding
+cd ../build
+make
+make test
+sudo make install
+sudo ldconfig
+cd ../apps
