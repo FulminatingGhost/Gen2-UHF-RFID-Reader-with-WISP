c5a036bbc0a7e7ff429af27d2fb5116b363c4594
Jeong Sin-Gi <jsgnwk@naver.com>
2019-02-15 17:46:04 +0900

    exp

--- gr-rfid/lib/reader_impl.cc
+++ gr-rfid/lib/reader_impl.cc
@@ -207,6 +207,7 @@ namespace gr
 
       gettimeofday (&reader_state-> reader_stats.end, NULL);
       suseconds_t execution_time = reader_state-> reader_stats.end.tv_usec - reader_state-> reader_stats.start.tv_usec;
+      result << reader_state-> reader_stats.end.tv_usec << " " << reader_state-> reader_stats.start.tv_usec << std::endl;
       result << "│ Execution time: " << execution_time << " (μs)" << std::endl;
       result << "│ Throughput(success EPC bits / time): " << reader_state->reader_stats.n_epc_correct * EPC_BITS / execution_time * 1e6 << " (bits/second)" << std::endl;
       result << "└──────────────────────────────────────────────────" << std::endl;
